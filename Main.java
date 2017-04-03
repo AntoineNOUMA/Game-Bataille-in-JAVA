2cc1706f57ef8f1ddc454688bc1cda8e52bbfc16
Ginks <pissabruney@hotmail.com>
2017-04-03 16:34:04 +0200

    Modification de la Main

--- Main.java
+++ Main.java
@@ -1,82 +1,76 @@
-
 import java.util.*;
 
 /**
- * 
- */
+*
+*/
 public class Main {
-
+	
+	
+	
 	/**
-	 * 
-	 * Représente le lot de carte contenu dans une main
-	 */
+	*
+	* Représente le lot de carte contenu dans une main
+	*/
 	public Deque<Carte> lotDeCarte;
-
+	
 	
 	
 	/**
-	 * @param c Collection de cartes provenant du jeu de carte
-	 */
+	* @param c Collection de cartes provenant du jeu de carte
+	*/
 	public Main(HashSet<Carte> c) {
 		this.lotDeCarte = new LinkedList<Carte>();
 		for(Carte p : c ){
 			this.lotDeCarte.add(p);
+			}
 		}
-	}
-
 	
-
+	
+	
 	/**
-	 * @return true si la main est vide
-	 */
-<<<<<<< HEAD
-
-	public boolean mainVide() 
-	{
-		// TODO implement here
-=======
+	* @return true si la main est vide
+	*/
 	public boolean mainVide() {
 		if(this.lotDeCarte.isEmpty()) return true;
 		return false;
->>>>>>> 89f6ae378be8f4686d037e0c04a66469a9423930
-	}
-
+		}
+	
 	/**
-	 * Méthode permettant de battre les cartes contenues dans la main
-	 */
+	* Méthode permettant de battre les cartes contenues dans la main
+	*/
 	public void battreMain() {
 		List<Carte>list = new ArrayList<Carte>();
 		for(Carte c : this.lotDeCarte){
 			list.add(c);
-		}
+			}
 		Collections.shuffle(list);
 		this.lotDeCarte = new ArrayDeque<Carte>(list);
-	}
+		}
 	
 	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
+	* @see java.lang.Object#toString()
+	*/
 	public String toString(){
 		String s;
 		int size=this.lotDeCarte.size();
 		s="La main contient " + size + "";
 		return s;
-	}
+		}
 	
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		
-		Carte n = new Carte("8", 'K');
+		Carte n = new Carte(8, 'K');
 		System.out.println(n);
 		System.out.println(n.getHauteur());
-		Carte m = new Carte("A", 'T');
+		Carte m = new Carte(14, 'T');
 		System.out.println(m);
 		System.out.println(m.getHauteur());
 		System.out.println(n.equals(m));
-		Carte t = new Carte("R", 'K');
+		Carte t = new Carte(12, 'K');
 		System.out.println(t);
 		System.out.println(t.getHauteur());
-		Carte x = new Carte("A", 'T');
+		Carte x = new Carte(14, 'T');
 		System.out.println(x);
 		System.out.println(x.getHauteur());
 		System.out.println(m.equals(x));
@@ -94,7 +88,8 @@ public class Main {
 		System.out.println(main.lotDeCarte.pop());
 		System.out.println(main.lotDeCarte.peekFirst());
 		System.out.println(main.lotDeCarte.peekLast());
-
+		
+		
+		}
+	
 	}
-
-}
