9044a3f9754e49a29b724e2cc52040fb45db495c
Ginks <pissabruney@hotmail.com>
2017-04-03 16:28:42 +0200

    Modification JeudeCartes

--- JeudeCartes.java
+++ JeudeCartes.java
@@ -1,35 +1,74 @@
 import java.util.*;
 
 /**
- * 
- */
-public class Jeu de Cartes {
-
+*
+*/
+public class JeudeCartes {
+	
+	
+	
 	/**
-	 * Default constructor
-	 */
-	public Jeu de Cartes() {
+	*
+	*/
+	private List<Carte> paquet;
+	
+	/**
+	* Default constructor
+	*/
+	public JeudeCartes() {
+		this.paquet= new ArrayList<Carte>();
+		}
+	
+	/**
+	* @param numJoueur
+	*/
+	public void distribuerCarte(Joueur joueur, Partie p) {
+		int i = this.paquet.size()/p.nbJoueur;
+		this.battrePaquet();
+		HashSet<Carte> h = new HashSet<Carte>();
+		for(int j = 0; j<i; j++){
+			h.add(this.paquet.get(j));
+			}
+		joueur.recupererCartes(h);
+		}
+	
+	
+	public void battrePaquet() {
+		List<Carte>list = new ArrayList<Carte>();
+		for(Carte c : this.paquet){
+			list.add(c);
+			}
+		Collections.shuffle(list);
+		this.paquet = new ArrayList<Carte>(list);
+		}
+	
+	/**
+	*
+	*/
+	public void creationPaquet(int nbCarte, int nbJeu) {
+		try{
+			int i, j;
+			for(i=0; i<nbJeu ; i++){
+				for(j=0; j<nbCarte; j++){
+					Carte c1, c2, c3, c4;
+					
+					c1 = new Carte(j+2, 'K');
+					c2 = new Carte(j+2, 'C');
+					c3 = new Carte(j+2, 'P');
+					c4 = new Carte(j+2, 'T');
+					this.paquet.add(c1);
+					this.paquet.add(c2);
+					this.paquet.add(c3);
+					this.paquet.add(c4);
+					}
+				}
+			}
+		catch(Exception e){}
+		}
+	
+	
+	
+	public String toString(){
+		return this.paquet.toString();
+		}
 	}
-
-	/**
-	 * 
-	 */
-	public HashSet<Carte> paquet;
-
-
-
-	/**
-	 * @param numJoueur
-	 */
-	public void distribuerCarte(int numJoueur) {
-		// TODO implement here
-	}
-
-	/**
-	 * 
-	 */
-	public void creationPaquet() {
-		// TODO implement here
-	}
-
-}
