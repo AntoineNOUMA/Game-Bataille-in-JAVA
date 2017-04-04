eee4023b79bd2d91cd5bcbd1d8c8ff08466d3c93
Ginks <pissabruney@hotmail.com>
2017-04-04 11:01:32 +0200

    Plateau Individuel

--- PlateauIndividuel.java
+++ PlateauIndividuel.java
@@ -1,35 +1,34 @@
 import java.util.*;
 
-public class PlateauIndividuel 
-{
-
-	private Carte cartePosee;
+/**
+*
+*/
+public class PlateauIndividuel {
+	private Stack<Carte> pile;
+	private HashSet<Carte> tas;
 	
-	private HashMap cartesRetournees;
-
-	/*Constructeur*/
-	public PlateauIndividuel() 
-	{
-		
-	}
-
+	public PlateauIndividuel(){
+		setPile(new Stack<Carte>());
+		setTas(new HashSet<Carte>());
+		}
 	
-
-
-
-
-	/**
-	 * 
-	 */
-	public void bataille() {
-		// TODO implement here
+	public Carte getPile() {
+		return this.pile.pop();
+		}
+	
+	public void setPile(Stack<Carte> pile) {
+		this.pile = pile;
+		}
+	
+	public HashSet<Carte> getTas() {
+		return tas;
+		}
+	
+	public void setTas(HashSet<Carte> tas) {
+		this.tas = tas;
+		}
+	
+	public void ajouterCarte(Carte c){
+		this.pile.add(c);
+		}
 	}
-
-	/**
-	 * 
-	 */
-	public void preparationBataille() {
-		// TODO implement here
-	}
-
-}
