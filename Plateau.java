650d73910e09f52ea3ddac54ce67798c6beb3a17
Ginks <pissabruney@hotmail.com>
2017-04-04 10:58:49 +0200

    Bataille

--- Plateau.java
+++ Plateau.java
@@ -1,41 +1,40 @@
 import java.util.*;
 
-public class Plateau 
-{
-	/**
-	 * Liste des cartes par joueurs dans une bataille
-	 */
-	public HashMap<Joueur, Carte> bataille;
-
-	/**
-	 * Liste des cartes retournées à un joueur
-	 */
-	public HashMap<Joueur, Carte> levee;
+/**
+*
+*/
+public class Plateau {
+	
+	
+	
 	
 	/**
-	 * 
-	 */
+	* Liste des cartes par joueurs dans une bataille
+	*/
+	private HashMap<Joueur, Carte> bataille;
+	
+	/**
+	* Liste des cartes retournées à un joueur
+	*/
+	private HashSet<Carte> levee;
+	
+	/**
+	*
+	*/
 	public Plateau() {
-	}
-
-
-	
-	public void bataille() 
-	{
-		// TODO implement here
-	}
-
-	public void preparationBataille() 
-	{
-		// TODO implement here
-	}
-
-	public int compareCarte(Carte c1, Carte c2) {
-		if(c1.compareTo(c2)==0)
-		{
-			return 0;
+		bataille=new HashMap<Joueur, Carte>();
+		levee = new HashSet<Carte>();
 		}
-		return 1;
+	
+	public HashSet<Carte> bataille() {
+		for(int i=0; i<this.bataille.size(); i++){}
+		return levee;
+		
+		}
+	
+	public void preparationBataille(HashSet<Joueur> players) {
+		for(Joueur j : players){
+			this.bataille.put(j, j.getMonPlateau().getPile());
+			}
+		}
 	}
-
-}
