0d2235c4a51b0a33c38b800d8383e2d8bf8a230d
Ginks <pissabruney@hotmail.com>
2017-06-05 14:16:07 +0200

    Update Parametre.java

--- graphique/Parametre.java
+++ graphique/Parametre.java
@@ -29,7 +29,6 @@ public class Parametre extends JDialog {
             }
         });
 
-        // call onCancel() when cross is clicked
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
@@ -37,7 +36,6 @@ public class Parametre extends JDialog {
             }
         });
 
-        // call onCancel() on ESCAPE
         contentPane.registerKeyboardAction(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 onCancel();
@@ -46,12 +44,10 @@ public class Parametre extends JDialog {
     }
 
     private void onOK() {
-        // add your code here
         dispose();
     }
 
     private void onCancel() {
-        // add your code here if necessary
         dispose();
     }
 
